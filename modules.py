d4db67527f1a6cb6dec83d125a549b34f80fdbf5
Halil İbrahim AYÇİÇEK <127198386+haycicek@users.noreply.github.com>
2025-02-21 20:42:13 +0300

    Create modules.py

---
+++ modules.py
@@ -0,0 +1,18 @@
+#alias
+from matematik import topla as toplamaIslemi
+from video2 import sayHello
+from video3 import Human
+#built-in modules
+import random
+from selenium import webdriver
+#package
+
+print(toplamaIslemi(10, 20))
+
+print(random.randint(0, 100))
+
+human1 = Human("İbrahim")
+human1.talk("Selamlar")
+
+chromeDriver = webdriver.Chrome()
+#packages
