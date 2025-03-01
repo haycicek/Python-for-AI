8c90cb8e47d2d8a75fceac13ccf3bb834256a544
Halil İbrahim AYÇİÇEK <ibrahim_aycicek@windowslive.com>
2025-03-01 17:44:02 +0300

    Create asal_mi.py

---
+++ week 2/asal_mi.py
@@ -0,0 +1,14 @@
+def asal_mi(sayi):
+    if sayi < 2:
+        print(f"{sayi} bir asal sayı değildir.")
+        return
+    for k in range(2, sayi):
+        if sayi % k == 0:
+            print(f"{sayi} bir asal sayı değildir.")
+            return
+    print(f"{sayi} bir asal sayıdır.")
+
+asal_mi(7)  # Çıktı: 7 bir asal sayıdır.
+asal_mi(10) # Çıktı: 10 bir asal sayı değildir.
+asal_mi(222222)
+asal_mi(771123)
